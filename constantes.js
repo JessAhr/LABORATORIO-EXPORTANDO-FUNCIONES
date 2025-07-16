4aff7b13901a16a379f2845ffcf7b1ca266c7dcc
karen M ulcue L <looshiri1@gmail.com>
2025-07-16 14:43:12 -0500

    pimer punto, exportar constantes

---
+++ constantes.js
@@ -0,0 +1,8 @@
+// En constantes.js, exportar:- PI = 3.1416- IVA = 0.19 (19%)- DIAS_SEMANA = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'
+
+export const Pi = 3.1416;
+export const IVA = 0.19;
+export const _DIAS_SEMANA = ['Lunes', 'Martes', 'Miércoles', 'Jueves', 'Viernes', 'Sábado', 'Domingo'];
+
+
+
